2555dea340e40ba05b58b56a0b2c4c95ef551992
Teemu Salminen <teemujsa@gmail.com>
2019-11-03 22:24:33 +0200

    Initial commit

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    /*
+     * Toteuta AnimationController -luokka:
+     * 
+     * 1. Hae GameObjectissa oleva Animator -komponentti. Voit käyttää GetComponent -funktiota.
+     * 2. Hae GameObjectissa oleva PlayerController -komponentti. Voit käyttää GetComponent -funktiota.
+     * 3. Käytä PlayerControllerin GetSpeed() -funktiota hakeaksesi pelaajan nopeuden.
+     * 4. Käytä PlayerControllerin IsGrounded() -funktiota tarkastaaksesi onko pelaaja maan päällä.
+     * 5. Animoi pelaaja asettamalla Animator -komponentin tilat seuraavilla ehdoilla. Voit käyttä Animator.Play() -funkiota tilan asettamiseksi:
+     *   - Kun pelaaja ei ole maan päällä, niin tilan tulee olla "Jump"
+     *   - Kun pelaajan nopeus on 0, tilan tulee olla "Idle"
+     *   - Kun pelaajan nopeus on 0 - 5, niin tilan tulee olla "Walk"
+     *     - Tilan ollessa "Walk", animaation nopeuden tulee olla puolet pelaajan nopeudesta (esim. jos pelaajan nopeus on 4 m/s, animaation nopeuden tulee olla 2)
+     *   - Kun pelaajan nopeus on suurempi kuin 5, tilan tulee olla "Run"
+     *     - Tilan ollessa "Run", animaation nopeuden tulee olla viidennes pelaajan nopeudesta (esim. jos pelaajan nopeus on 6 m/s, animaation nopeuden tulee olla 1.2)
+     *     
+     * VINKKI: Animaation nopeutta voit säätää muuttamalla Animator.speed -kenttää!
+     */
+}
+
+class PlayerAnimatorStates
+{
+    public const string Run = "Run";
+    public const string Walk = "Walk";
+    public const string Idle = "Idle";
+    public const string Jump = "Jump";
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxVelocity = 10;
+    public float timeToMax = 5;
+
+    private CharacterController characterController;
+    private Animator animator;
+    private float velocity = 0;
+    private Vector3 horizontalDirection;
+    private Vector3 speed;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 currentDirection = Vector3.zero;
+        if (Input.GetKey("w"))
+            currentDirection += Vector3.forward;
+        if (Input.GetKey("a"))
+            currentDirection += Vector3.left;
+        if (Input.GetKey("s"))
+            currentDirection += Vector3.back;
+        if (Input.GetKey("d"))
+            currentDirection += Vector3.right;
+        if(currentDirection.magnitude > 0)
+        {
+            horizontalDirection = currentDirection;
+            if (velocity < maxVelocity)
+                velocity = Mathf.Min(maxVelocity, velocity + maxVelocity * (Time.deltaTime / timeToMax));
+        }
+        if (currentDirection.magnitude == 0)
+            velocity = Mathf.Max(0, velocity - maxVelocity * (Time.deltaTime / (timeToMax / 2)));
+        transform.LookAt(transform.position + horizontalDirection);
+
+        Vector3 horizontalSpeed = horizontalDirection * velocity;
+        speed = new Vector3(horizontalSpeed.x, speed.y, horizontalSpeed.z);
+        if (characterController.isGrounded)
+        {
+            speed.y = 0;
+            if (Input.GetKeyDown("space"))
+            {
+                speed.y = 10;
+            }
+        }
+        speed.y -= 9 * Time.deltaTime;
+        characterController.Move(speed * Time.deltaTime);
+
+        if(transform.position.y < -10)
+        {
+            transform.position = startPosition;
+            speed = Vector3.zero;
+            velocity = 0;
+        }
+
+    }
+
+    public float GetSpeed()
+    {
+        return velocity;
+    }
+
+    public bool IsGrounded()
+    {
+        return characterController.isGrounded;
+    }
+}
